60bc3081e1ecc7bb35685fec1f09d4be215b32e2
LukasJurena <jurenalukash@gmail.com>
2024-02-15 13:29:40 +0100

    adding database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-5xl">
-      <h1>To Do List</h1>
-    </main>
+      <div>
+          <Navbar/>
+          <div className="flex flex-row py-4 space-x-4">
+              <Card title={"Todo"} description={"moje první Todo"} checked={true}/>
+              <Card title={"Todo"} description={"moje první Todo"} checked={true}/>
+              <Card title={"Todo"} description={"moje první Todo"} checked={true}/>
+          </div>
+      </div>
+
   );
+}
+class TodoItemProps{
+    title:string;
+    description:string;
+    checked:Boolean;
+}
+function Navbar(){
+    return<nav className="flex justify-between bg-cyan-500 ">
+        <label className="hover:text-orange-400 px-4">Stránka všechny Todos</label>
+        <label className="hover:text-orange-400 px-4">Přidej Todos</label>
+    </nav>
+}
+function Card({title, description, checked}:TodoItemProps){
+    return <div className="flex flex-col items-center justify-start bg-slate-300 w-1/6 h-60 px-4 hover:bg-slate-600">
+
+        <h2 className="text-xl py-1">{title}</h2>
+        <label className="text-xs">{description}</label>
+    </div>
 }
